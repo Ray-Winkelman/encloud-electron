2d4d9a99907402ee2b755901f13753016eee7344
Ray Winkelman <rwinkelman@beyondtrust.com>
2016-12-04 00:23:38 -0400

    Versioning script + bootstrap.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 const express = require('express');
-
+const port = 3000;
 
 /* --- Electron Injection Point --- */
 const {app, BrowserWindow} = electron;
@@ -9,13 +9,13 @@ app.on('ready', function() {
 
   let win = new BrowserWindow({
     width: 800,
-    height: 400,
+    height: 600,
     autoHideMenuBar: true,
     useContentSize: true,
     resizable: false,
   });
 
-  win.loadURL('http://localhost:3000/');
+  win.loadURL(`http://localhost:${port}/`);
   win.focus();
 
 });
@@ -24,12 +24,14 @@ app.on('ready', function() {
 /* --- Express Routes --- */
 var e = express();
 
+e.use(express.static(__dirname));
 e.set('view engine', 'ejs');
 
+
 e.listen(3000, function () {
-  console.log('Express is listening on port 3000.')
+  console.log(`Express is listening on port ${port}.`)
 })
 
 e.get('/', function(req, res){
-  res.render('encloud', {view: 'partials/test'});
+  res.render('encloud', {view: 'index'});
 });
